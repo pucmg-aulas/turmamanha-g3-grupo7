8e7b2dd56ec824a3268cb62c2f0cefcc8f9aa51c
Gustavo Ignácio <169084532+keijizn@users.noreply.github.com>
2024-11-10 18:01:44 -0300

    Add files via upload

---
+++ Estacionamento/src/model/VagaModel.java
@@ -0,0 +1,53 @@
+package model;
+
+public abstract class VagaModel {
+    protected String id;
+    protected boolean ocupada;
+    protected VeiculoModel veiculo;
+
+    protected static double precoPorFacao = 4.0;
+    protected static double limite = 50.0;
+
+    public VagaModel(String id) {
+        this.id = id;
+        this.ocupada = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void ocuparVaga(VeiculoModel veiculo) {
+        this.veiculo = veiculo;
+        this.ocupada = true;
+    }
+
+    public void liberarVaga() {
+        this.veiculo = null;
+        this.ocupada = false;
+    }
+
+    public VeiculoModel getVeiculo() {
+        return veiculo;
+    }
+
+    public abstract double calcularPreco(int minutos);
+    public abstract String getTipo();
+
+    public static double getPrecoPorFacao() {
+        return precoPorFacao;
+    }
+
+    public static double getLimite() {
+        return limite;
+    }
+
+    // Novo método para definir o estado de ocupação
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+}
