bd7c21d9b2c642423bfa1f2dba7294e2d16664f4
Gustavo Ignácio <169084532+keijizn@users.noreply.github.com>
2024-11-10 13:51:13 -0300

    Add files via upload

---
+++ codigo/Estacionamento/src/model/VagaVIP.java
@@ -0,0 +1,22 @@
+package model;
+
+public class VagaVIP extends Vaga {
+
+    protected static double taxa = 0.87;
+
+    public VagaVIP(String id) {
+        super(id);
+    }
+    
+    @Override
+    public String getTipo() {
+        return "VIP";
+    }
+
+
+    @Override
+    public double calcularPreco(int minutos) {
+        // Usa os valores estáticos precoporfacao e limite da classe Vaga
+        return Math.min((minutos / 15) * precoporfacao * taxa, limite);
+    }
+}
