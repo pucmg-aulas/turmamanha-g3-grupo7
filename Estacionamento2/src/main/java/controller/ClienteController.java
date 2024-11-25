5da03e5ab5c7d424ecdf47a6007a8c89e7ff8d32
Diogo Henrique Moreira da Silva <164037438+Diogo-Henrique-Moreira-da-Silva@users.noreply.github.com>
2024-11-25 16:39:25 -0300

    Filtro ranking

--- Estacionamento2/src/main/java/controller/ClienteController.java
+++ Estacionamento2/src/main/java/controller/ClienteController.java
@@ -14,35 +14,29 @@ public class ClienteController {
         this.clienteDAO = new ClienteDAO();
     }
 
-    // Adiciona um cliente e retorna o objeto criado
     public ClienteModel adicionarCliente(String nome) {
         ClienteModel cliente = new ClienteModel(nome, null); // O ID será gerado pelo DAO
         clienteDAO.salvarCliente(cliente);
         return cliente;
     }
 
-    // Adiciona um veículo a um cliente
     public void adicionarVeiculoAoCliente(String idCliente, String placa) {
         VeiculoModel veiculo = new VeiculoModel(placa);
         clienteDAO.adicionarVeiculoAoCliente(idCliente, veiculo);
     }
 
-    // Retorna a lista de todos os clientes e seus veículos
     public List<ClienteModel> listarTodosClientes() {
         return clienteDAO.listarTodos();
     }
 
-    // Remove um cliente pelo ID
     public void removerCliente(String idCliente) {
         clienteDAO.removerCliente(idCliente);
     }
 
-    // Busca um cliente pelo ID
     public ClienteModel buscarClientePorId(String id) {
         return clienteDAO.buscarPorId(id);
     }
 
-    // Busca um cliente pela placa de um veículo
     public ClienteModel buscarClientePorPlaca(String placa) {
         return clienteDAO.buscarClientePorPlaca(placa);
     }
@@ -51,9 +45,8 @@ public class ClienteController {
         return clienteDAO.listarTicketsDoCliente(idCliente);
     }
 
-    public List<Object[]> obterRankingClientes() {
-        return clienteDAO.calcularRankingClientes();
+    public List<ClienteModel> listarRankingClientes(int mes, int ano) {
+        return clienteDAO.obterRankingClientes(mes, ano);
     }
-
 
 }
